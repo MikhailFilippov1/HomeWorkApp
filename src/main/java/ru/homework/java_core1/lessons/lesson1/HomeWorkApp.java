1d09494d86a813bba6bd4304a9ddf66de4d358f7
MikhailFilippov1 <cp_filippov@mail.ru>
2021-10-22 10:29:19 +0300

    Initial commit

---
+++ src/main/java/ru/homework/java_core1/lessons/lesson1/HomeWorkApp.java
@@ -0,0 +1,39 @@
+package ru.homework.java_core1.lessons.lesson1;
+
+public class HomeWorkApp {
+
+    public static void main(String[] args){
+        printThreeWords();
+        checkSumSign();
+        printColor();
+        Numbers();
+    }
+        public static void printThreeWords(){
+            System.out.println("Orange" + '\n' + "Banana" + '\n' + "Apple");
+        }
+
+        public static void checkSumSign(){
+            int a = -10;
+            int b = 2;
+            int sum = a + b;
+                if(sum >= 0){
+                    System.out.println("Сумма положительная");
+                } else{
+                    System.out.println("Сумма отрицательная");
+                }
+        }
+
+        public static void printColor(){
+            int value = -2;
+                if(value <= 0) System.out.println("Красный");
+                    else if(value > 0 && value <= 100)System.out.println("Желтый");
+                        else System.out.println("Зеленый");
+        }
+
+        public static void Numbers(){
+            int a = 10;
+            int b = 11;
+                if(a >= b)System.out.println("a >= b");
+                    else System.out.println("a < b");
+        }
+}
