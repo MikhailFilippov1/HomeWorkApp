3ae18a3b8cb70b863cd01d579696d3e3cdfe525c
MikhailFilippov1 <cp_filippov@mail.ru>
2021-10-26 14:22:30 +0300

    Домашнее задание №2

---
+++ src/main/java/ru/homework/java_core1/lessons/lesson2/HomeWorkApp2.java
@@ -0,0 +1,58 @@
+package ru.homework.java_core1.lessons.lesson2;
+import java.util.Scanner;
+
+public class HomeWorkApp2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println(excersize1(10,20));
+
+        printArgumentValueIntoConsole(-2);
+
+        System.out.println(excersize2(10));
+
+        printSomethingIntoConsole("Hello", 5);
+
+        int year;
+        System.out.println("\nПРОГРАММА ОПРЕДЕЛЕНИЯ ВИСОКОСНОГО ГОДА");
+
+            System.out.println("Введите год:");
+            year = scanner.nextInt();
+            if(defineLeapYear(year) == true)System.out.println("Год високосный\n");
+                else System.out.println("Год невисокосный\n");
+
+    }
+
+    public static boolean excersize1(int a, int b){
+        int result = a + b;
+        if(result >= 10 && result <= 20)return true;
+        else return false;
+    }
+
+    public static void printArgumentValueIntoConsole(int a){
+        if(a >= 0)System.out.println("Число положительное");
+            else System.out.println("Число отрицательное");
+    }
+
+    public static boolean excersize2(int a){
+        if(a > 0)return false;
+        else return true;
+    }
+
+    public static void printSomethingIntoConsole(String s, int a) {
+        for (int i = 0; i < a; i++) {
+            System.out.println(s);
+        }
+    }
+
+    public static boolean defineLeapYear(int year) {
+        int x;
+        if(year %4 == 0) {
+            x = year%100;
+            if(year %400 == 0)return true;
+                else if(x != 0)return true;
+                    else return false;
+        }
+        else return false;
+    }
+}
